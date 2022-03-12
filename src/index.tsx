f6de3a6bedc83758a86a310ebff598d7847fc9ff
AdiDD <adi.deaconu21@gmail.com>
2022-03-12 13:46:37 +0200

    Create Code Editor component

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import * as esbuild from "esbuild-wasm";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 
+import CodeEditor from "./components/code-editor";
+
 const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
@@ -63,6 +65,10 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
