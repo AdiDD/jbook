9910d2417debeb9edcbec74552a947c6ac7e953e
AdiDD <adi.deaconu21@gmail.com>
2022-03-13 18:39:27 +0200

    Create preview component

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import "bulmaswatch/superhero/bulmaswatch.min.css";
 import * as esbuild from "esbuild-wasm";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import Preview from "./components/preview";
 
 import CodeEditor from "./components/code-editor";
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
   const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
 
   useEffect(() => {
     startService();
@@ -27,7 +28,6 @@ const App = () => {
     if (!ref.current) return;
 
     // Reinitialize iframe before every submit, to make sure everything works as expected
-    iframe.current.srcdoc = html;
 
     const result = await ref.current.build({
       entryPoints: ["index.js"],
@@ -40,29 +40,8 @@ const App = () => {
       },
     });
 
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener("message", (event) => {
-            try {
-              eval(event.data);
-            } catch(err) {
-              const root = document.querySelector("#root");
-              root.innerHTML = "<div style='color: red;'><h4>Runtime error</h4>" + err + "</div>";
-              console.error(err);
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div>
@@ -70,19 +49,10 @@ const App = () => {
         initialValue="const a = 1;"
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        ref={iframe}
-        title="Code iframe"
-        sandbox="allow-scripts"
-        srcDoc={html}
-      />
+      <Preview code={code} />
     </div>
   );
 };
